5bce4019542f816109f5b062f38296e6b53da652
Abbey Ames (she/her) <98247246+abbeyames@users.noreply.github.com>
2022-04-14 01:43:24 +0000

    Updates to calendar loading

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ currentUser = {}
 def index():
   return render_template("index.html")
 
+@app.route('/db', methods=['POST', 'GET'])
+def db_info():
+  error = '';
+  if request.method == 'GET':
+    error = db.keys();
+  return render_template('db.html', error=error)
+  
 # https://flask.palletsprojects.com/en/2.0.x/quickstart/
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-  error = None
+  error = '';
   if request.method == 'POST':
     valid_login = attempt_login(request.form['email'], request.form['password'])
     if valid_login == 'login':
       login_user(request.form['email'])
-      return redirect(url_for('main'))
+      return render_template("main.html") # should be redirect instead
     elif valid_login == 'incorrect password':
       error = 'Incorrect password'
     else:
@@ -44,7 +51,7 @@ def login_user(email):
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-  error = None
+  error = '';
   if request.method == 'POST':
     usedEmail = existing_email(request.form['email'])
     if usedEmail == False:
@@ -54,7 +61,7 @@ def signup():
                          request.form['lastName'],
                          request.form['jobTitle'])
       create_account(request.form['email'], user)
-      return redirect(url_for('login'))
+      return render_template("login.html") # should be redirect instead
     else:
       error = "An account with this email already exists"
   return render_template("signup.html", error=error)
@@ -87,10 +94,45 @@ def create_account(email, userData):
 def main():
   return render_template("main.html")
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
+@app.route('/main/schedule', methods=['POST', 'GET'])
+def schedule():
+  if request.method == 'POST':
+    event = create_event(request.form['eventTitle'],
+                        request.form['startDate'],
+                        request.form['endDate'],
+                        request.form['recurring'],
+                        request.form['description'],
+                        request.form['mentorAvailability'])
+    if request.form['recurring'] == 'no':
+      currentUser['singularEvents'].append(event)
+    else:
+      currentUser['recurringEvents'].append(event)
+    update_user_data()
+  
+  return render_template("main.html")
+
+def create_event(title, start, end, recurring, description, mentor):
+  event = {
+    "eventTitle": title,
+    "startDate": start,
+    "endDate": end,
+    "recurring": recurring,
+    "description": description,
+    "mentorAvailability": mentor
+  }
+  return event
+  
+def update_user_data():
+  global currentUser
+  db[currentUser["email"]] = currentUser
+  print("User info updated")
+
+@app.route('/logout')
+def logout():
+  global currentUser
+  currentUser = {}
+  print("User logged out")
+  return render_template("index.html") # should be redirect
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
@@ -145,17 +187,17 @@ app.run(host='0.0.0.0', port=8080, debug=True)
 
 # we also need to remove the data once they have logged out
 
-def logout_user():
-  global currentUser
-  currentUser = {}
-  print("User logged out")
+# def logout_user():
+#   global currentUser
+#   currentUser = {}
+#   print("User logged out")
 
 # whenever a user changes data, we need to save it to their profile
 
-def update_user_data():
-  global currentUser
-  db[currentUser["email"]] = currentUser
-  print("User info updated")
+# def update_user_data():
+#   global currentUser
+#   db[currentUser["email"]] = currentUser
+#   print("User info updated")
 
 # check to see if when a user is attempting to create an account, the email does not already exist in the database
   
@@ -165,3 +207,4 @@ def update_user_data():
 #     return True
 #   except:
 #     return False
+
